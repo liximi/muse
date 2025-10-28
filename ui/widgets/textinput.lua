2f5069835e15c6d7195575dae5bcfd489ea376cb
liximi <973695015@qq.com>
2025-10-28 22:54:38 +0800

    完善文本输入框代码，现在在输入回车时会报错

--- ui/widgets/textinput.lua
+++ ui/widgets/textinput.lua
@@ -6,6 +6,10 @@ local Utils = require "ui.utils"
 local addHoverState = require "ui.components".addHoverState
 
 
+local cursor_blinking, cursor_blinking_timer = true, 0
+local cursor_blinking_duration, cursor_blinking_duration_half = 1, 0.5
+
+
 local TextInput = Class(Widget, function(self, datas, theme)
 	datas = datas or {}
 	Widget.new(self, "TextInput", datas, theme)
@@ -23,7 +27,6 @@ local TextInput = Class(Widget, function(self, datas, theme)
 		text_color = datas.text_color or self.theme.text_color,
 		anchors = {0, 0, 1, 1},
 		padding = datas.text_padding or {0, 0, 0, 0},
-		pivot = {0.5, 0.5},
 	}))
 	if datas.hint then
 		self.hint = self:addChild(Text({
@@ -31,13 +34,12 @@ local TextInput = Class(Widget, function(self, datas, theme)
 			text_color = datas.hint_color or self.theme.hint_color,
 			anchors = {0, 0, 1, 1},
 			padding = datas.text_padding or {0, 0, 0, 0},
-			pivot = {0.5, 0.5},
 		}))
 	end
 
 	self.cursor = {
 		show = false,
-		pos = {1, 0},--{行, 当行的字符索引值}
+		index = 1,
 		_local_pos_cache = {0, 0}--相对本地坐标系的坐标（左上角为文本的原点，考虑padding）
 	}
 
@@ -74,17 +76,24 @@ function TextInput:showCursor(show)
 	end
 end
 
-function TextInput:setCursorPos(line, index)
-	local font = self.text:getFont()
-	local text = self.text:getText(true)
-	local maxw, wrappedtext = font:getWrap(text, self.text.transform.w)
-	if line then
-		line = math.max(1, math.min(#wrappedtext, line))--clamp
-		self.cursor.pos[1] = line
+function TextInput:setCursorPos(index)
+	if not index then
+		return
 	end
-	if index then
-		index = math.max(0, math.min(utf8.len(wrappedtext[line or self.cursor.pos[1]]), index))--clamp
-		self.cursor.pos[2] = index
+
+	local text = self.text:getText(true)
+	self.cursor.index = math.max(0, math.min(#text+1, index))--clamp
+
+	local font = self.text:getFont()
+	local maxw, wrappedtext = font:getWrap(text, self.text.transform.w)
+	local line = 1
+	for l, s in ipairs(wrappedtext) do
+		local len = #s
+		if len + 1 >= index then
+			line = l
+			break
+		end
+		index = index - len
 	end
 
 	if not text or text == "" then
@@ -93,8 +102,7 @@ function TextInput:setCursorPos(line, index)
 	end
 	local line_height = font:getHeight() * font:getLineHeight()
 	self.cursor._local_pos_cache[2] = line_height * (line - 1)
-	local byteoffset = utf8.offset(wrappedtext[line], -1)
-	self.cursor._local_pos_cache[1] = font:getWidth(string.sub(wrappedtext[line], 1, byteoffset))
+	self.cursor._local_pos_cache[1] = font:getWidth(string.sub(wrappedtext[line], 1, index-1))
 end
 
 function TextInput:setCursorPosByScreenPos(screen_x, screen_y)
@@ -103,10 +111,43 @@ function TextInput:setCursorPosByScreenPos(screen_x, screen_y)
 	local text = self.text:getText(true)
 	local maxw, wrappedtext = font:getWrap(text, self.text.transform.w)
 	local line_height = font:getHeight() * font:getLineHeight()
-	local line = math.min(math.ceil(local_y/line_height), #wrappedtext)
-	self.cursor.pos[1] = line
-
-	--TODO
+	local line = math.max(math.min(math.ceil(local_y/line_height), #wrappedtext), 1)
+	local line_text = wrappedtext[line]
+	local final_x = 0
+	local final_line_idx = 1
+	if line_text then
+		for pos, code in utf8.codes(line_text) do
+			local len = font:getWidth(string.sub(line_text, 1, pos - 1))
+			if len >= local_x then
+				if len - final_x > (len - local_x) * 2 then
+					final_x = len
+					final_line_idx = pos
+				end
+				break
+			end
+			final_x = len
+			final_line_idx = pos
+		end
+		local len = font:getWidth(line_text)
+		if len >= local_x then
+			if len - final_x > (len - local_x) * 2 then
+				final_x = len
+				final_line_idx = #line_text+1
+			end
+		else
+			final_x = len
+			final_line_idx = #line_text+1
+		end
+	end
+	for l, s in ipairs(wrappedtext) do
+		if l >= line then
+			break
+		end
+		final_line_idx = final_line_idx + #s
+	end
+	self.cursor.index = final_line_idx
+	self.cursor._local_pos_cache[1] = final_x
+	self.cursor._local_pos_cache[2] = line_height * (line - 1)
 end
 
 
@@ -124,12 +165,15 @@ function TextInput:onMousePressed(x, y, button)
 	end
 	local is_in_scope = self:regionDetection(x, y)
 	local is_focus = self:isFocus()
-	if is_in_scope and not is_focus then
+	if is_in_scope then
 		self:setCursorPosByScreenPos(x, y)
-		self:setFocus()
-		return
+		cursor_blinking_timer = 0
+		if not is_focus then
+			self:setFocus()
+			return
+		end
 	end
-	if is_focus then
+	if not is_in_scope and is_focus then
 		self:removeFocus()
 		self:onHovered(is_in_scope)
 	end
@@ -168,9 +212,12 @@ function TextInput:onTextInput(text)
 	if not self:isFocus() then
 		return
 	end
-	local old_text = self.text:getText()
-	local new_text = old_text..text
+	local old_text = self.text:getText(true)
+	local old_idx = self.cursor.index
+	print(old_idx, #old_text)
+	local new_text = table.concat({string.sub(old_text, 1, old_idx - 1), text, string.sub(old_text, old_idx)})
 	self.text:setText(new_text)
+	self:setCursorPos(old_idx + #text)
 	if self.height_adaptive then
 		self:refreashHeight()
 	end
@@ -178,8 +225,6 @@ function TextInput:onTextInput(text)
 end
 
 
-local cursor_blinking, cursor_blinking_timer = true, 0
-local cursor_blinking_duration, cursor_blinking_duration_half = 2, 1
 function TextInput:onUpdate(dt)
 	cursor_blinking_timer = cursor_blinking_timer + dt
 	if cursor_blinking_timer > cursor_blinking_duration then
@@ -196,6 +241,21 @@ end
 function TextInput:onPostDraw()
 	if self.cursor.show and cursor_blinking then
 		--绘制光标
+		local org_x, org_y, w, h, r = self.text.transform:getGlobalBounds()
+		local top_x, top_y = org_x + self.cursor._local_pos_cache[1], org_y + self.cursor._local_pos_cache[2]
+		local bottom_y = top_y + self.text:getFont():getHeight()
+
+		love.graphics.push()
+			if r ~= 0 and r ~= Utils.TWO_PI then
+				local px, py = self.text.transform:getGlobalPosition()
+				love.graphics.translate(px, py)
+				love.graphics.rotate(r)
+				love.graphics.translate(-px, -py)
+			end
+			love.graphics.setColor(1, 1, 1, 1)
+			love.graphics.setLineWidth(1)
+			love.graphics.line(top_x, top_y, top_x, bottom_y)
+		love.graphics.pop()
 	end
 end
 
